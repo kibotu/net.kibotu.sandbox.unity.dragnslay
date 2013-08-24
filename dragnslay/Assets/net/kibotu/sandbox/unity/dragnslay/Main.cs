e780e2f9b67c3ab272c99ee23d4a6ba338878e89
kibotu <janrabe@beuth-hochschule.de>
2013-08-24 19:15:18 +0200

    button + socket client start

--- dragnslay/Assets/net/kibotu/sandbox/unity/dragnslay/Main.cs
+++ dragnslay/Assets/net/kibotu/sandbox/unity/dragnslay/Main.cs
@@ -1,19 +1,32 @@
 using UnityEngine;
+using System;
 using System.Collections;
+using SocketIOClient;
 
 public class Main : MonoBehaviour {
 	
 	public Texture btnTexture;
+	private bool buttonIsVisible = true;
+	private Client socket;
 	
     void OnGUI() {
         if (!btnTexture) {
             Debug.LogError("Please assign a texture on the inspector");
             return;
         }
-        if (GUI.Button(new Rect(10, 10, 50, 50), btnTexture))
+        if (buttonIsVisible && GUI.Button(new Rect(10, 10, 50, 50), btnTexture)) {
             Debug.Log("Clicked the button with an image");
-        
+			buttonIsVisible = false;
+			startGame();
+		}
     }
+	
+	void startGame() {
+		socket = new Client("http://172.16.3.13:3000");
+		socket.Connect();
+		
+		
+	}
 	
 	void Start () {
 	
