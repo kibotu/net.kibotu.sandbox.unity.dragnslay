af3cba64d566969292d658d019f078fb6a82eb92
Jan Rabe <jan.rabe@wooga.net>
2013-09-14 15:55:12 +0200

    updated copy script

--- tools/unity_export_copy_script.bat
+++ tools/unity_export_copy_script.bat
@@ -2,6 +2,7 @@
 
 set PATH=%~dp0msys\bin;%~dp0win-rsync;%PATH%
 
-sh copy_script.sh WIN unity_export dragnslay-android/unity_export/dragnslay dragnslay-android/dragnslay
+sh copy_script.sh WIN unity_export dragnslay-android/unity_export/dragnslay/assets dragnslay-android/dragnslay/assets
+sh copy_script.sh WIN unity_export dragnslay-android/unity_export/dragnslay/libs dragnslay-android/dragnslay/libs
 
 PAUSE
