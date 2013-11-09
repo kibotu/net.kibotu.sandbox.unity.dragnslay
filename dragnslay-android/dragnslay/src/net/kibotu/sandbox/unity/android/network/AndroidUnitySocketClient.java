19aa1b87e58c7e569f9ad9243eadfd0eb7aef451
Jan Rabe <jan.rabe@wooga.net>
2013-11-09 19:18:37 +0100

    socket io

--- dragnslay-android/dragnslay/src/net/kibotu/sandbox/unity/android/network/AndroidUnitySocketClient.java
+++ dragnslay-android/dragnslay/src/net/kibotu/sandbox/unity/android/network/AndroidUnitySocketClient.java
@@ -3,50 +3,50 @@ package net.kibotu.sandbox.unity.android.network;
 import android.util.Log;
 import com.koushikdutta.async.http.socketio.Acknowledge;
 import com.unity3d.player.UnityPlayer;
-import net.kibotu.sandbox.chat.client.android.network.SocketHandler;
+import net.kibotu.sandbox.chat.client.android.network.SocketClient;
 import org.json.JSONArray;
 import org.json.JSONObject;
 
 /**
  * send message to unity: UnityPlayer.UnitySendMessage("GameObjectName1", "MethodName1", "Message to send");
  */
-public class AndroidUnitySocketHandler extends SocketHandler {
+public class AndroidUnitySocketClient extends SocketClient {
 
-    private static final String TAG = AndroidUnitySocketHandler.class.getSimpleName();
+    private static final String TAG = AndroidUnitySocketClient.class.getSimpleName();
 
     @Override
     protected void EventCallback(final JSONArray argument, final Acknowledge acknowledge) {
         Log.v(TAG, "onEvent 'send' ack = " + acknowledge.toString() + " args = " + argument.toString());
-        UnityPlayer.UnitySendMessage("AndroidSocketHandler", "StringCallback", argument.toString());
+        UnityPlayer.UnitySendMessage("AndroidSocketClient", "StringCallback", argument.toString());
     }
 
     @Override
     protected void StringCallback(final String message, final Acknowledge acknowledge) {
         Log.v(TAG, "onString " + message + " " + acknowledge);
-        UnityPlayer.UnitySendMessage("AndroidSocketHandler", "StringCallback", message);
+        UnityPlayer.UnitySendMessage("AndroidSocketClient", "StringCallback", message);
     }
 
     @Override
     protected void JSONCallback(final JSONObject jsonObject, final Acknowledge acknowledge) {
         Log.v(TAG, "onJson " + jsonObject + " " + acknowledge);
-        UnityPlayer.UnitySendMessage("AndroidSocketHandler", "JSONCallback", jsonObject.toString());
+        UnityPlayer.UnitySendMessage("AndroidSocketClient", "JSONCallback", jsonObject.toString());
     }
 
     @Override
     protected void DisconnectCallback(final Exception e) {
         Log.v(TAG, "onDisconnect " + e.getMessage());
-        UnityPlayer.UnitySendMessage("AndroidSocketHandler", "DisconnectCallback", e.getMessage());
+        UnityPlayer.UnitySendMessage("AndroidSocketClient", "DisconnectCallback", e.getMessage());
     }
 
     @Override
     protected void ErrorCallback(final String error) {
         Log.v(TAG, "onError " + error);
-        UnityPlayer.UnitySendMessage("AndroidSocketHandler", "ErrorCallback", error);
+        UnityPlayer.UnitySendMessage("AndroidSocketClient", "ErrorCallback", error);
     }
 
     @Override
     protected void ReconnectCallback() {
         Log.v(TAG, "onReconnect");
-        UnityPlayer.UnitySendMessage("AndroidSocketHandler", "ReconnectCallback", null);
+        UnityPlayer.UnitySendMessage("AndroidSocketClient", "ReconnectCallback", null);
     }
 }
