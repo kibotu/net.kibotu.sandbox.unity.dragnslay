cd82ac57ec7d8aaaf1d45903495cad5a7ec8c33a
Jan Rabe <jan.rabe@wooga.net>
2013-11-30 19:13:07 +0100

    unity binary export is changing each deploy -> not supposed to be in repo

--- android/unity_export/dragnslay/gen/net/kibotu/sandbox/unity/android/dragnslay/BuildConfig.java
+++
@@ -1,8 +0,0 @@
-/*___Generated_by_IDEA___*/
-
-/** Automatically generated file. DO NOT MODIFY */
-package net.kibotu.sandbox.unity.android.dragnslay;
-
-public final class BuildConfig {
-    public final static boolean DEBUG = true;
-}
--- android/unity_export/dragnslay/gen/net/kibotu/sandbox/unity/android/dragnslay/R.java
+++
@@ -1,7 +0,0 @@
-/*___Generated_by_IDEA___*/
-
-package net.kibotu.sandbox.unity.android.dragnslay;
-
-/* This stub is for using by IDE only. It is NOT the R class actually packed into APK */
-public final class R {
-}
