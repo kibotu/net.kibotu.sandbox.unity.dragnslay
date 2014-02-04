b67527c96123114351ad2965cd7ee5ddc8e2e08b
janpaschen <janp1988@gmail.com>
2014-02-04 16:41:08 +0100

    Skybox done

--- unity/Assets/Src/net/kibotu/sandbox/unity/dragnslay/menu/view/ShopView.cs
+++ unity/Assets/Src/net/kibotu/sandbox/unity/dragnslay/menu/view/ShopView.cs
@@ -3,42 +3,35 @@
 namespace Assets.Src.net.kibotu.sandbox.unity.dragnslay.menu.view
 {
     public class ShopView : MonoBehaviour
-    {
+    {   
+        float scale = 1f;
         public void Start()
         {
             var atlas = GameObject.Find("shop_atlas").GetComponent<UIToolkit>();
 
             var sprite = UIButton.create(atlas, "center.png", "center.png", 0, 0);
             sprite.positionCenter();
-        //    sprite.scale = new Vector3(0.52083f, 0.520833f, 0);
+            sprite.scale = new Vector3(scale, scale, 1);
 
             sprite = UIButton.create(atlas, "buy.png", "buy.png", 0, 0);
-            sprite.positionFromBottom(0.2f);
-          //  sprite.scale = new Vector3(0.52083f, 0.520833f, 0);
-
-          //  sprite = UIButton.create(atlas, "left.png", "left.png", 0, 0);
-          //  sprite.positionFromLeft(0.27f);
-          //  sprite.scale = new Vector3(0.52083f, 0.520833f, 0);
-
-          //  sprite = UIButton.create(atlas, "right.png", "right.png", 0, 0);
-         //   sprite.positionFromRight(0.27f);
-          //  sprite.scale = new Vector3(0.52083f, 0.520833f, 0);
+            sprite.positionFromCenter(0.39f,0f);
+            sprite.scale = new Vector3(scale, scale, 1);
 
             sprite = UIButton.create(atlas, "Skins.png", "Skins.png", 0, 0);
-            sprite.positionFromCenter(-0.2f,-0.21f);
-           // sprite.scale = new Vector3(0.52083f, 0.520833f, 0);
+            sprite.positionFromCenter(-0.4f,-0.375f);
+            sprite.scale = new Vector3(scale, scale, 1);
 
             sprite = UIButton.create(atlas, "boosts.png", "boosts.png", 0, 0);
-            sprite.positionFromCenter(-0.2f, -0.07f);
-         //   sprite.scale = new Vector3(0.52083f, 0.520833f, 0);
+            sprite.positionFromCenter(-0.4f, -0.125f);
+            sprite.scale = new Vector3(scale, scale, 1);
 
             sprite = UIButton.create(atlas, "upgrades.png", "upgrades.png", 0, 0);
-            sprite.positionFromCenter(-0.2f, 0.07f);
-       //     sprite.scale = new Vector3(0.52083f, 0.520833f, 0);
+            sprite.positionFromCenter(-0.4f, 0.125f);
+            sprite.scale = new Vector3(scale, scale, 1);
 
             sprite = UIButton.create(atlas, "buffs.png", "buffs.png", 0, 0);
-            sprite.positionFromCenter(-0.2f, 0.21f);
-         //   sprite.scale = new Vector3(0.52083f, 0.520833f, 0);
+            sprite.positionFromCenter(-0.4f, 0.375f);
+            sprite.scale = new Vector3(scale, scale, 1);
         }
     }
 }
