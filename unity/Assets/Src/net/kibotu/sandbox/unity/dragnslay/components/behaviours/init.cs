f0eccc677594785158b1ea125737cc77822a3d76
Jan Rabe <jan.rabe@wooga.net>
2014-02-11 16:50:07 +0100

    re-imported assets

---
+++ unity/Assets/Src/net/kibotu/sandbox/unity/dragnslay/components/behaviours/init.cs
@@ -0,0 +1,20 @@
+﻿using Assets.Src.net.kibotu.sandbox.unity.dragnslay.game;
+using UnityEngine;
+using System.Collections;
+
+public class init : MonoBehaviour
+{
+    public GameObject source;
+    public GameObject target;
+
+    void OnGUI()
+    {
+        if (GUILayout.Button("Start Bounce"))
+        {
+
+
+
+
+        }
+    }
+}
