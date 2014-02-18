71c890d6e61259d9f3b497f608664a4f6df0ed8c
Jan Rabe <jan.rabe@wooga.net>
2014-02-18 19:46:12 +0100

    awesome rotations with twists and everything

--- unity/Assets/Sources/components/behaviours/Orbiting.cs
+++ unity/Assets/Sources/components/behaviours/Orbiting.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections;
+using HutongGames.PlayMaker.Actions;
 using UnityEngine;
 using Random = UnityEngine.Random;
 
@@ -13,60 +15,93 @@ namespace Assets.Sources.components.behaviours
         public Transform center;
         public float circulationTerm;
         public Quaternion rotation;
-        private float time;
+        private float _time;
 
         public Vector2 heightvariance;
         public float heightchangetime;
-        private float randomHeight;
-        private float heighttime;
-        private float lastyoffset;
+        private float _randomHeight;
+        private float _heighttime;
+        private float _lastyoffset;
         public float height;
         public float strafeAngle;
+
 
         public void Start() {
             if (Math.Abs(yoffset) < Epsilon) yoffset = Random.Range(-2f, 2f);
             if(center == null) center = transform.parent;
             if (Math.Abs(radius) < Epsilon) radius = Random.Range(5f, 10f);
-            if(rotation ==null) rotation = Quaternion.identity;
-            if (Math.Abs(circulationTerm) < Epsilon) circulationTerm = Random.Range(4f, 10f);
-            if(heightvariance == null) heightvariance = new Vector2(-2f, 2f);
-            if (Math.Abs(heightchangetime) < Epsilon) heightchangetime = circulationTerm / 2f; // 4f;
+            if (Math.Abs(circulationTerm) < Epsilon) circulationTerm = Random.Range(4f, 12f);
+            if(heightvariance == Vector2.zero) heightvariance = new Vector2(-2f, 2f);
+            if (Math.Abs(heightchangetime) < Epsilon) heightchangetime = circulationTerm / 6f; // 4f;
             if (Math.Abs(strafeAngle) < Epsilon) strafeAngle = 10f;
+            if(ChanceOfRandomTwist == Vector2.zero) ChanceOfRandomTwist = new Vector2(5f, 10f);
+            if (Math.Abs(TwistDuration) < Epsilon) TwistDuration = 1.125f;
+            _twistTime = TwistDuration;
+            _randomTwistChance = Random.Range(ChanceOfRandomTwist.x, ChanceOfRandomTwist.y);
         }
+
+        public Vector2 ChanceOfRandomTwist;
+        public float TwistDuration;
+        private float _randomTwistChance;
+        private float _twistTime;
+        private float _randomTwistChanceTime;
 
         public void FixedUpdate()
         {
             if (center == null) return;
-            time += Time.deltaTime;
-            heighttime += Time.deltaTime;
+            _time += Time.deltaTime;
+            _heighttime += Time.deltaTime;
+            _twistTime += Time.deltaTime;
 
             transform.position = GetFinalDestination();
             transform.rotation = GetFinalRotation();
+
+            if (_twistTime <= TwistDuration)
+            {
+                strafeAngle = strafeAngle = Mathf.Lerp(10, 370, Easing.Sinusoidal.easeInOut(_twistTime / TwistDuration));
+            }
+            else
+            {
+                _randomTwistChanceTime += Time.deltaTime;
+                if (_randomTwistChanceTime > _randomTwistChance)
+                {
+                    _randomTwistChanceTime = 0;
+                    _twistTime = 0;
+                    _randomTwistChance = Random.Range(ChanceOfRandomTwist.x, ChanceOfRandomTwist.y);
+                } 
+            }
+        }
+
+        private static float GetStrafeAngle(float dt, float duration)
+        {
+            // http://www.wolframalpha.com/input/?i=360+*+sin+%28x*6%29
+            return -180f * Mathf.Cos(dt*6f/duration) + 180f; 
         }
 
         public Vector3 GetFinalDestination()
         {
             height = ComputeHeightVariation();
-            return RotateAroundCenterY(transform.position, time, radius, center.position, circulationTerm, height);
+            return RotateAroundCenterY(transform.position, _time, radius, center.position, circulationTerm, height);
         }
 
         private float ComputeHeightVariation()
         {
-            if (heighttime > heightchangetime)
+            if (_heighttime > heightchangetime)
             {
-                heighttime = 0;
-                lastyoffset = randomHeight;
-                randomHeight = Random.Range(heightvariance.x, heightvariance.y) + yoffset;
+                _heighttime = 0;
+                _lastyoffset = _randomHeight;
+                _randomHeight = Random.Range(heightvariance.x, heightvariance.y) + yoffset;
             }
-            return Mathf.Lerp(lastyoffset, randomHeight, heighttime / heightchangetime);
+            return Mathf.Lerp(_lastyoffset, _randomHeight, _heighttime / heightchangetime);
         }
 
         public Quaternion GetFinalRotation()
+
         {
             // beware this is the most complicated way i could come up with to rotate the plane along the tangent of the circle
-            Vector3 targetDir = RotateAroundCenterY(transform.position, (time + circulationTerm / 360), radius, center.position, circulationTerm, height) - transform.position;
-            float step = 1000 * Time.deltaTime;
-            Vector3 newDir = Vector3.RotateTowards(transform.forward, targetDir, step, 0.0F);
+            var targetDir = RotateAroundCenterY(transform.position, (_time + circulationTerm / 360), radius, center.position, circulationTerm, height) - transform.position;
+            var step = 1000 * Time.deltaTime;
+            var newDir = Vector3.RotateTowards(transform.forward, targetDir, step, 0.0F);
             //Debug.DrawRay(transform.position, newDir, Color.red);
             return Quaternion.LookRotation(newDir) * Quaternion.Euler(0, 0, -strafeAngle);//*Quaternion.Euler(-90f, 0, 0) * Quaternion.Euler(0,0,90) *;
         }
@@ -89,7 +124,7 @@ namespace Assets.Sources.components.behaviours
 
         public static float Phi(float t, float tu)
         {
-            return (Mathf.Abs(tu) < 0.0001f) ? 2f * Mathf.PI * t : 2f * Mathf.PI * t / tu;
+            return (Mathf.Abs(tu) < Epsilon) ? 2f * Mathf.PI * t : 2f * Mathf.PI * t / tu;
         }
     }
 }
---
+++ unity/Assets/Sources/playmaker/actions/ToggleEnable.cs
@@ -0,0 +1,18 @@
+﻿using HutongGames.PlayMaker;
+
+namespace Assets.Sources.playmaker.actions
+{
+    [ActionCategory(ActionCategory.ScriptControl)]
+    public class ToggleEnable : FsmStateAction {
+
+        public override void Reset()
+        {
+            
+        }
+
+        public override void OnEnter()
+        {
+            Finish();
+        }
+    }
+}
