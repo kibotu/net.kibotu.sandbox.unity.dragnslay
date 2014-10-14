6663c26a1e909b50b34d6980d824af4b5b8ab5c6
Jan Rabe <jan.rabe@wooga.net>
2014-10-14 12:06:53 +0200

    working prefab buttons + intro scene using 4.6 ui

--- unity/Assets/Sources/menu/Menu.cs
+++ unity/Assets/Sources/menu/Menu.cs
@@ -1,5 +1,9 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections;
+using Assets.Sources.menu.transition;
+using HutongGames.PlayMaker.Actions;
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace Assets.Sources.menu
 {
@@ -14,8 +18,6 @@ namespace Assets.Sources.menu
         public GameObject LoadingScreen;
         public GameObject Upgrades;
 
-        private readonly List<GameObject> _active = new List<GameObject>();
-
         private static Menu _instance;
 
         public Menu Shared
@@ -23,59 +25,110 @@ namespace Assets.Sources.menu
             get { return _instance ?? (_instance = new Menu()); }
         }
 
-        private void ClearAll()
+        public void Remove(GameObject go)
         {
-            foreach (var o in _active)
-            {
-                Destroy(o);
-            }
-            _active.Clear();
+            Destroy(go);
+        }
+
+        public void LoadLevel(String level)
+        {
+            Application.LoadLevelAdditiveAsync(level);
         }
 
         public void ShowMainMenu()
         {
-            ClearAll();
-            _active.Add(Instantiate(MainMenu) as GameObject);
+            var go = Instantiate(MainMenu) as GameObject;
+            const float fadeInTime = 3f;
+            foreach (var image in go.GetComponentsInChildren<Image>())
+            {
+                if (image.gameObject.name == "Banner")
+                    image.ScaleOverTime(new Vector3(0.35f, 0.35f, 0.35f), new Vector3(0.7f, 0.7f, 0.7f), 5f);
+
+                if (image.gameObject.name == "Profile" || 
+                    image.gameObject.name == "Market" || 
+                    image.gameObject.name == "Invite Friends" ||
+                    image.gameObject.name == "Play Online" ||
+                    image.gameObject.name == "Play Saga" ||
+                    image.gameObject.name == "Settings")
+                    image.ScaleOverTime(new Vector3(0.3f, 0.2f, 0.2f), new Vector3(0.4f, 0.4f, 0.4f), 5f);
+
+
+                image.CrossFadeAlpha(0.0f,0,false);
+                image.CrossFadeAlpha(1f, fadeInTime, false);
+            }
+
+            foreach (var text in go.GetComponentsInChildren<Text>())
+            {
+                text.CrossFadeAlpha(0.0f, 0, false);
+                text.CrossFadeAlpha(1f, fadeInTime, false);
+            }
         }
+
+       
 
         public void ShowGameHud()
         {
-            ClearAll();
-            _active.Add(Instantiate(Hud) as GameObject);
+            
         }
 
         public void ShowMarket()
         {
-            ClearAll();
-            _active.Add(Instantiate(Market) as GameObject);
         }
 
         public void ShowProfile()
         {
-            ClearAll();
-            _active.Add(Instantiate(Profile) as GameObject);
         }
 
         public void ShowUpgrades()
         {
-            ClearAll();
-            _active.Add(Instantiate(Upgrades) as GameObject);
         }
 
         public void ShowWinScreen()
         {
-            _active.Add(Instantiate(WinScreen) as GameObject);
         }
 
         public void ShowLoseScreen()
         {
-            _active.Add(Instantiate(LoseScreen) as GameObject);
         }
 
         public void ShowLoadingScreen()
         {
-            ClearAll();
-            _active.Add(Instantiate(LoadingScreen) as GameObject);
+        }
+
+        public void ShowInviteFriendsScreen()
+        {
+        }
+
+        public void PlaySagaScreen()
+        {
+        }
+
+        public void PlayOnlineScreen()
+        {
+        }
+
+        public void ShowSettings()
+        {
+        }
+    }
+
+    public static partial class TransitionExtentions
+    {
+        public static void ScaleOverTime(this Image image, Vector3 from, Vector3 to, float time)
+        {
+            image.StartCoroutine(image.rectTransform.ScaleOverTime(from, to, time));
+        }
+
+        public static IEnumerator ScaleOverTime(this RectTransform rect, Vector3 from, Vector3 to, float time)
+        {
+            float startTime = 0;
+            rect.localScale = from;
+            while (Math.Abs(rect.localScale.magnitude - to.magnitude) > Mathf.Epsilon)
+            {
+                startTime += Time.deltaTime;
+                rect.localScale = Vector3.Lerp(from, to, startTime / time);
+                yield return new WaitForEndOfFrame();
+            }
         }
     }
 }
